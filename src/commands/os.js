412bbcd147909ecdef48afe862522f92782a6c75
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-11 19:45:07 +0300

    feat: implement os --cpus command

--- src/commands/os.js
+++ src/commands/os.js
@@ -1,4 +1,4 @@
-import { EOL } from 'os';
+import { cpus, EOL } from 'os';
 
 const os = async payload => {
     const arg = payload.args[0].slice(2);
@@ -6,6 +6,18 @@ const os = async payload => {
     switch (arg) {
         case 'EOL':
             console.log(JSON.stringify(EOL));
+            break;
+        case 'cpus':
+            const cpuList = cpus().map(cpu => {
+                const cpuView = {
+                    model: cpu.model,
+                    speed: cpu.speed
+                };
+
+                return cpuView;
+            });
+
+            console.log(cpuList);
             break;
     }
 };
